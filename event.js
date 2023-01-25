13e016147e1fe228c985bc843902b2711e31025c
CHINTAN-9925 <111982890+CHINTAN-9925@users.noreply.github.com>
2023-01-25 22:21:44 +0530

    Add files via upload

---
+++ event.js
@@ -0,0 +1,15 @@
+function clearScreen() {
+    document.getElementById("display").value = "";
+}
+
+// This function display values
+function display(value) {
+    document.getElementById("display").value += value;
+}
+
+// This function evaluates the expression and returns result
+function calculate() {
+    var p = document.getElementById("display").value;
+    var q = eval(p);
+    document.getElementById("display").value = q;
+}
